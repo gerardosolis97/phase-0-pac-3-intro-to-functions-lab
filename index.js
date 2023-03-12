ef9e918e59b88dd07f70782cb193b3d63731ab0d
Gerardo Solis <gerardosolis327@gmail.com>
2023-03-12 18:49:39 -0500

    complete lab

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    // todo
+    return string.toUpperCase();
+  }
+  
+  function whisper(string) {
+    // todo
+    return string.toLowerCase();
+  } 
+
+  function logShout(string) {
+    // todo
+    console.log(string.toUpperCase( ));
+  } 
+  
+  function logWhisper(string) {
+    // todo
+    console.log(string.toLowerCase( ));
+  }
+
+  function sayHiToHeadphonedRoommate(string) {
+    
+    if (string === "hello") 
+    return "I can\'t hear you!";
+
+    else if (string === "HELLO") 
+    return "YES INDEED!";
+    
+    else if (string === "Let\'s have dinner together!") 
+    return "I would love to!"
+  }
